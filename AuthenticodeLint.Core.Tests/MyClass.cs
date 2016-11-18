c0ca09c2716e5617d2000df8f0241dff3fdae2fe
Kevin Jones <kevin@vcsjones.com>
2016-11-18 12:08:20 -0500

    Project layout.

---
+++ AuthenticodeLint.Core.Tests/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AuthenticodeLint.Core.Tests
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+	}
+}
---
+++ AuthenticodeLint.Core/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AuthenticodeLint.Core
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+	}
+}
