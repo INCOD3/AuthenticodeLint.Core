3fc4ed4a4740445ed2d3d3e4207c8c65132d37d7
Kevin Jones <kevin@vcsjones.com>
2016-11-18 16:47:57 -0500

    Added a simple test that runs for PE reading.

--- AuthenticodeLint.Core.Tests/PortableExecutableTests.cs
+++ AuthenticodeLint.Core.Tests/PortableExecutableTests.cs
@@ -1,7 +1,28 @@
 ﻿using System;
 using Xunit;
+using AuthenticodeLint.Core.PE;
+using System.Threading.Tasks;
 
 namespace AuthenticodeLint.Core.Tests
 {
-	
+	public class Tests
+	{
+		[Fact]
+		public async Task ShouldReadSimpleAttributesOfPE()
+		{
+			using (var pe = new PortableExecutable("files/authlint.exe"))
+			{
+				var header = await pe.GetDosHeader();
+				Assert.NotEqual(0, header.ExeFileHeaderAddress);
+
+				var peHeader = await pe.GetPeHeader(header);
+				Assert.Equal(MachineArchitecture.x86, peHeader.Architecture);
+				Assert.Equal(16, peHeader.DataDirectories.Count);
+
+				var securityHeader = peHeader.DataDirectories[ImageDataDirectoryEntry.IMAGE_DIRECTORY_ENTRY_SECURITY];
+				Assert.NotEqual(0, securityHeader.Size);
+				Assert.NotEqual(0, securityHeader.VirtualAddress);
+			}
+		}
+	}
 }
